42237b34f6080338c58c6b4c6b37712eb423dac3
marci <jackjack2000.kahp@gmail.com>
2024-06-24 15:59:37 +0700

    add git ignore

--- hr/app/core/src/main/java/net/marci/app/server/ServerAppConfig.java
+++ hr/app/core/src/main/java/net/marci/app/server/ServerAppConfig.java
@@ -1,2 +1,29 @@
-package net.marci.app.server;public class ServerAppConfig {
+package net.marci.app.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(
+  basePackages = {
+    "net.marci.module.config",
+    "net.marci.server.http",
+  }
+)
+@EnableConfigurationProperties
+@SpringBootApplication(
+  exclude = { SecurityAutoConfiguration.class  }
+)
+@Import(value = {
+  /* TODO: Implement these classes
+  WebSecurityConfig.class,
+  WebResourceConfig.class,
+  WebSocketConfig.class
+   */
+})
+public class ServerAppConfig {
 }
--- hr/employee/src/main/java/net/marci/hr/entity/Employee.java
+++ hr/employee/src/main/java/net/marci/hr/entity/Employee.java
@@ -1,2 +1,11 @@
-package net.marci.hr.entity;public class Employee {
+package net.marci.hr.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.marci.BaseEntity;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Employee extends BaseEntity {
 }
