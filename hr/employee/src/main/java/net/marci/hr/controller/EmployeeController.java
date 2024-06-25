fa8cbf12bc87342caa4f7b95856337b4b8fbc610
marci <jackjack2000.kahp@gmail.com>
2024-06-25 09:40:27 +0700

    add employee controller

--- hr/employee/src/main/java/net/marci/hr/controller/EmployeeController.java
+++ hr/employee/src/main/java/net/marci/hr/controller/EmployeeController.java
@@ -1,2 +1,25 @@
-package net.marci.hr.controller;public class EmployeeController {
+package net.marci.hr.controller;
+
+import net.marci.hr.EmployeeService;
+import net.marci.hr.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/hr/employee")
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeService service;
+
+  @GetMapping("/{id}")
+  public Employee getById(@PathVariable(value = "id") long id) {
+    return service.getById(id);
+  }
+
+  @PostMapping("/create")
+  public Employee create(@RequestBody Employee employee) {
+    return service.save(employee);
+  }
 }
