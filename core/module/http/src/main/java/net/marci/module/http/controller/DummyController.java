f0b4f751b36edde595a34dd6aed24bf22a0ff0d6
marci <jackjack2000.kahp@gmail.com>
2024-07-20 15:50:02 +0700

    add Dummy controller: RESTful testing ground

--- core/module/http/src/main/java/net/marci/module/http/controller/DummyController.java
+++ core/module/http/src/main/java/net/marci/module/http/controller/DummyController.java
@@ -1,2 +1,19 @@
-package net.marci.module.http.controller;public class DummyController {
+package net.marci.module.http.controller;
+
+import net.marci.module.http.dto.ServerResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.Callable;
+
+@RestController
+@RequestMapping("/dummy")
+public class DummyController extends BaseController {
+
+  @PostMapping("/hello")
+  @ResponseStatus(HttpStatus.OK)
+  public @ResponseBody ServerResponse hello() {
+    Callable<String> executor = () -> "Welcome to Marci Server";
+    return this.execute("dummy", "hello", executor);
+  }
 }
