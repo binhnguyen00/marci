b424567a386aae2d6290a9f403f44f293b86806b
marci <jackjack2000.kahp@gmail.com>
2024-07-22 17:22:58 +0700

    add popup page

--- core/ui/lib/src/widget/popup/UIPopup.tsx
+++ core/ui/lib/src/widget/popup/UIPopup.tsx
@@ -1,36 +1,26 @@
 import React from "react";
 import { Popup as ReactPopup } from "reactjs-popup";
-
-declare type PopupPosition = 
-  'top left' | 'top center' | 'top right' | 
-  'right top' | 'right center' | 'right bottom' | 
-  'bottom left' | 'bottom center' | 'bottom right' | 
-  'left top' | 'left center' | 'left bottom' | 
-  'center center';
+import { PopupPosition } from "reactjs-popup/dist/types";
 
 declare interface PopupProps {
   position: PopupPosition | PopupPosition[];
   popupAsPage?: boolean; // Modal
-  children?: React.ReactNode;
+  popupItem?: React.ReactNode;
   className?: string;
-  disabled?: boolean;
 }
 
 export function Popup(props: PopupProps) {
-  let { position, className, disabled, children, popupAsPage } = props;
-
-  if (disabled === undefined) disabled = false;
-  if (popupAsPage === undefined) popupAsPage = false;
+  let { className = "", popupAsPage = false, popupItem, position } = props;
 
   return (
-    <div className={className ? className : ""}>
+    <div className={className}> 
       <ReactPopup 
-        trigger={<button> Click to open popup </button>} 
+        open={true}
+        closeOnDocumentClick
         position={position} 
-        children={children ? children : undefined}
-        disabled={disabled}
+        children={popupItem ? popupItem : undefined}
         modal={popupAsPage}
       />
     </div>
-  );
+  )
 }
