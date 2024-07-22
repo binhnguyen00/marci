470b9726b1b6bfee21d46f67e74fa393536694d5
marci <jackjack2000.kahp@gmail.com>
2024-07-22 14:50:27 +0700

    add dummy controller & service for test purpose

--- core/module/http/src/main/java/net/marci/module/http/DummyService.java
+++ core/module/http/src/main/java/net/marci/module/http/DummyService.java
@@ -1,2 +1,11 @@
-package net.marci.module.http;public class DummyService {
+package net.marci.module.http;
+
+import org.springframework.stereotype.Service;
+
+@Service("DummyService")
+public class DummyService {
+
+  public String helloWorld() {
+    return "Welcome to Marci Server";
+  }
 }
--- core/module/http/src/main/java/net/marci/module/http/controller/DummyController.java
+++ core/module/http/src/main/java/net/marci/module/http/controller/DummyController.java
@@ -1,6 +1,8 @@
 package net.marci.module.http.controller;
 
+import net.marci.module.http.DummyService;
 import net.marci.module.http.dto.ServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,13 @@ import java.util.concurrent.Callable;
 @RequestMapping("/dummy")
 public class DummyController extends BaseController {
 
+  @Autowired
+  DummyService dummyService;
+
   @PostMapping("/hello")
   @ResponseStatus(HttpStatus.OK)
   public @ResponseBody ServerResponse hello() {
-    Callable<String> executor = () -> "Welcome to Marci Server";
-    return this.execute("dummy", "hello", executor);
+    Callable<String> executor = () -> dummyService.helloWorld();
+    return this.execute("DummyService", "helloWorld", executor);
   }
 }
