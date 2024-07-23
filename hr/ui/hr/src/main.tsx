7c3b2d9abc53e64bc11b396157b645db6aa7c287
marci <jackjack2000.kahp@gmail.com>
2024-07-23 07:50:02 +0700

    ensure document 'root' element

--- hr/ui/hr/src/main.tsx
+++ hr/ui/hr/src/main.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { UIHome } from "home/UIHome";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <UIHome />
-  </React.StrictMode>
-)
+let root = document.getElementById("root");
+
+function ensureRoot(root: HTMLElement | null): HTMLElement {
+  if (!root) {
+    root = document.createElement("div");
+    root.setAttribute("id", "root");
+    document.body.appendChild(root);
+  }
+  return root;
+}
+
+function renderRoot() {
+  root = ensureRoot(root);
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <UIHome />
+    </React.StrictMode>
+  )
+}
+
+renderRoot();
