ce37b39c3666e7438068a214b9b0b0bc0ecb5fc6
marci <jackjack2000.kahp@gmail.com>
2024-08-15 17:12:56 +0700

    Update code UI: delegate department to parent

--- hr/ui/hr/src/department/UIDepartmentList.tsx
+++ hr/ui/hr/src/department/UIDepartmentList.tsx
@@ -3,7 +3,6 @@ import * as icon from "react-icons/bs";
 import { widget, server, hook, input } from "@marci-ui/lib";
 import { IListProps } from "../interface/IListProps";
 import { IFormProps } from "../interface/IFormProps";
-import { ListUtils } from "../utilities/ListUtils";
 
 interface UIDepartmentListProps extends IListProps {}
 export function UIDepartmentList({ title = "Departments", height, onModify }: UIDepartmentListProps) {
@@ -11,22 +10,53 @@ export function UIDepartmentList({ title = "Departments", height, onModify }: UI
   const [ sqlArgs, setSqlArgs ] = React.useState<any>(widget.initSqlArgs());
   const [ reload, setReload ] = React.useState(false);
 
+  const renderCellGetRecordById = (record: any, index: number) => {
+
+    const successCB: server.CallBack = (response: server.ServerResponse) => {
+      const javaEntity = response.body as any;
+      const html = (
+        <UIDepartmentForm 
+          entity={javaEntity} reloadParent={reloadTable}/>
+      );
+      widget.createPopup(`Department: ${javaEntity.name}`, html);
+    }
+
+    const failCB: server.CallBack = (response: server.ServerResponse) => {
+      const error = response.message;
+      const html = (<div> {JSON.stringify(error, null, 2)} </div>)
+      widget.closeCurrentPopup();
+      widget.createPopup("Fail", html);
+    }
+
+    const createDepartment = () => {
+      const html = (
+        <UIDepartmentForm 
+          entity={undefined} parentId={record.id}  reloadParent={reloadTable}/>
+      );
+      widget.createPopup("New Department", html);
+    }
+
+    return ( 
+      <div className="flex-h justify-content-between" style={{ width: "100%" }}>
+        <div 
+          className="cell-clickable"
+          onClick={() => {
+            server.rpc.call("DepartmentService", "getById", { id: record.id }, successCB, failCB);
+          }}
+        > 
+          {record.name} 
+        </div>
+
+        <icon.BsPlus className="m-1" style={{ cursor: "pointer" }} onClick={createDepartment}/>
+      </div>
+    )
+  }
+
   const columns: widget.DataTableColumn[] = [ 
     { 
       field: "name", header: "Title", 
       customRender(record, index) {
-        return ListUtils.renderCellGetRecordById({
-          id: record.id,
-          cellValue: record.name,
-          rpcRequest: {
-            component: "DepartmentService",
-            service: "getById",
-          },
-          callBack(entity) {
-            const html = (<UIDepartmentForm entity={entity} reloadParent={reloadTable}/>);
-            widget.createPopup(`Department: ${entity.name}`, html);
-          },
-        });
+        return renderCellGetRecordById(record, index);
       },
     },
     { field: "description", header: "Description", width: 300 },
@@ -66,8 +96,10 @@ export function UIDepartmentList({ title = "Departments", height, onModify }: UI
   )
 }
 
-interface UIDepartmentFormProps extends IFormProps {}
-export function UIDepartmentForm({ entity = {}, reloadParent = () => {} }: UIDepartmentFormProps) {
+interface UIDepartmentFormProps extends IFormProps {
+  parentId?: number;
+}
+export function UIDepartmentForm({ entity = {}, reloadParent = () => {}, parentId }: UIDepartmentFormProps) {
   const [ departmentState, setDepartmentState ] = React.useState(entity);
   const isNewEntity = !entity.id;
 
@@ -83,7 +115,7 @@ export function UIDepartmentForm({ entity = {}, reloadParent = () => {} }: UIDep
       widget.closeCurrentPopup();
       reloadParent();
     };
-    server.rpc.call("DepartmentService", "save", { department: departmentState }, successCB);
+    server.rpc.call("DepartmentService", "save", { department: { ...departmentState, parentId: parentId } }, successCB);
   }
 
   return (
