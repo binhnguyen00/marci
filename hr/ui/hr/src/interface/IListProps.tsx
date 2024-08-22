aafa8bfcd9d3f3271de4fcb8c1089e58c28dec46
marci <jackjack2000.kahp@gmail.com>
2024-08-22 11:24:47 +0700

    clean code

--- hr/ui/hr/src/interface/IListProps.tsx
+++ hr/ui/hr/src/interface/IListProps.tsx
@@ -6,12 +6,15 @@
  * @property {string} [title] - [Optional] The title of the list.
  * @property {number} [height] - [Optional] The height of the list.
  * @property {function} [onModify] - [Optional] A callback function to be called when the list is modified.
+ * @property {any} [sqlArgs] - [Optional] The SQL arguments send to the server.
  */
 export interface IListProps {
   title?: string;
   height?: number;
   onModify?: (field: string, newValue: any, rollbackValue: any) => void;
   sqlArgs?: any;
+
+  // Consider enahance these property
   isSelector?: boolean;
   selectRowsCallBack?: (selectedRecords: any[]) => void;
 }
